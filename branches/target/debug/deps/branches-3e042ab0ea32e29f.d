56f9166f09059553d72d7941c5880fdad7f29006
Sajid Rizvi <67055492+BakerStreetPhantom@users.noreply.github.com>
2022-09-12 01:42:13 +0530

    Delete branches/target directory

--- branches/target/debug/deps/branches-3e042ab0ea32e29f.d
+++
@@ -1,5 +0,0 @@
-/Users/sajidrizvi/projects/branches/target/debug/deps/branches-3e042ab0ea32e29f.rmeta: src/main.rs
-
-/Users/sajidrizvi/projects/branches/target/debug/deps/branches-3e042ab0ea32e29f.d: src/main.rs
-
-src/main.rs:
--- branches/target/debug/deps/branches-dc80490243fe97e2.d
+++
@@ -1,5 +0,0 @@
-/Users/sajidrizvi/projects/branches/target/debug/deps/branches-dc80490243fe97e2.rmeta: src/main.rs
-
-/Users/sajidrizvi/projects/branches/target/debug/deps/branches-dc80490243fe97e2.d: src/main.rs
-
-src/main.rs:
